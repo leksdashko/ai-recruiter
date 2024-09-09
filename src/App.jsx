155d4573d225243a3657f641985d510de4553dc9
Oleksandr Dashko <leksdashko@gmail.com>
2024-09-09 15:02:24 +0200

    add react routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Updated for v6
+import HomePage from './pages/HomePage';
+import InterviewPage from './pages/InterviewPage';
+import SummaryPage from './pages/SummaryPage';
 
-import { Main, Description } from "./components";
+{/* <Main /> */}
+{/* <Description /> */}
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-				<Main />
-				<Description />
-      </div>
-    </BrowserRouter>
-  );
-}
+	return (
+		<BrowserRouter>
+			<div className='relative z-0 bg-primary'>
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/interview" element={<InterviewPage />} />
+				<Route path="/summary" element={<SummaryPage />} />
+			</Routes>
+			</div>
+		</BrowserRouter>
+	);
+};
 
 export default App;
+
